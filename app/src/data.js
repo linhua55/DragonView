b2f735da55fc73c32f96fe3c78bd4b6c2a30ce1f
Yarden Livnat <yarden@sci.utah.edu>
2015-10-05 21:36:29 -0600

    sync

--- app/src/data.js
+++ app/src/data.js
@@ -18,7 +18,7 @@ define(function(require) {
     this.groups = [];
     this.routers = d3.map();
     this.routers = d3.map();
-    this.nodes = new Map();
+    //this.nodes = new Map();
     this.counters = [];
     this.blues = [];
     this.greens = [];
@@ -86,15 +86,15 @@ define(function(require) {
     data.forEach(function (item) {
       rank++;
       if (item.core == undefined || item.core == 0) {
-        if (item.core == undefined) {
-          node_rank = rank;
-        } else {
-          node_rank = Math.floor(rank/24);
-          if (rank % 24 > 0)
-            console.log('rank issue:', rank, node_rank)
-        }
-
-        item.rank = node_rank;
+        //if (item.core == undefined) {
+        //  node_rank = rank;
+        //} else {
+        //  node_rank = Math.floor(rank/24);
+        //  if (rank % 24 > 0)
+        //    console.log('rank issue:', rank, node_rank)
+        //}
+        //
+        //item.rank = node_rank;
         item.g = +item.g;
         item.r = +item.r;
         item.c = +item.c;
@@ -111,7 +111,7 @@ define(function(require) {
           run.jobs.set(item.jobid, job);
           idx++;
         }
-        run.nodes.set(item.rank, item);
+        //run.nodes.set(item.rank, item);
 
         router = run.routers.get(model.router_id(item));
         if (router.jobs.indexOf(job) == -1) {
